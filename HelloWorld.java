4f4e5926eac8a927011338aa701874e8ac3e04f2
sainath711 <sainathsaii007@gmail.com>
2024-01-11 17:58:18 +0530

    new file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,6 @@
 
-public class sai {
+public class HelloWorld {
     public static void main(String[] args) {
-int a=10;
-int b=20;
-int c=a+b;
         System.out.println("Hello, World!");
     }
 }
