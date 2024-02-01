0a23d62d68126d138d04413e55e19fc2e8f82e13
sainath711 <sainathsaii007@gmail.com>
2024-02-01 13:32:22 +0530

    java

---
+++ operators/Testfive.java
@@ -0,0 +1,47 @@
+class Testone{
+	public static void main(String[] args){
+	int a=10;
+	int b=20;
+	int c=a+b;
+	System.out.println(c);
+	} 
+}
+class testtwo{
+	public static void main(String[] args){
+		int a=10;
+		int b=20;
+		int c=b-a;
+		System.out.println(c);
+	}
+	
+}
+
+
+class testthree{
+	public static void main(String[] args){
+		int a=10;
+		int b=20;
+		int c=a*b;
+		System.out.println(c);
+	}
+}
+
+class testfour{
+	public static void main(String[] args){
+		int a=10;
+		int b=20;
+		int c=b/a;
+		System.out.println(c);
+	}
+}
+
+
+class testfive{
+	public static void main(String[] args){
+		int a=10;
+		int b=20;
+		int c=b%a;
+		System.out.println(c);
+	}
+}
+
---
+++ operators/Testfour.java
@@ -0,0 +1,9 @@
+class Testone{
+	public static void main(String[] args){
+		int a=5;
+   //	int b =5++; we cant perform operation on value or literals (5) it must be a variable
+	    int b=a++;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
