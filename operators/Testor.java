1cf1955b3f88754f39115413c4617fca404de02e
sainath711 <sainathsaii007@gmail.com>
2024-02-02 13:27:51 +0530

    commit

---
+++ operators/Testor.java
@@ -0,0 +1,14 @@
+class Test{
+	public static void main(String[] args){
+	int a=10;
+	int b=20;
+	
+	if(a<b || b<a){
+		System.out.println("true");
+	}
+	else{
+		System.out.println("false");
+	}
+	
+	}
+}
