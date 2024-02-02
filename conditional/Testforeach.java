60b32e2cad54a53496b567e421209f40f555e18c
sainath711 <sainathsaii007@gmail.com>
2024-02-02 16:40:14 +0530

    java

---
+++ conditional/Testforeach.java
@@ -0,0 +1,13 @@
+class Test{
+public static void main(String[] args){
+	int[] ids={100,200,300,400};
+	String[] enames={"rahul","sonia","priya","modi"};
+	for(int id:ids){
+	System.out.println(id);
+	}
+	for(String ename:enames){
+	System.out.println(ename);
+	
+	}
+	}
+}	
---
+++ conditional/Testternaryop.java
@@ -0,0 +1,7 @@
+class Test{
+	public static void main(String[] args){
+	int i=4500;
+	 String msg=i>=100 && i<=999 ? "yes -3 digit no" : "not a 3 digit no";
+	 System.out.println(msg);
+	}
+}
