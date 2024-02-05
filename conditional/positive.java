b0fb341a1c3f974771e966a0e03418cf376986b7
sainath711 <sainathsaii007@gmail.com>
2024-02-05 16:09:16 +0530

    java

--- conditional/positive.java
+++ conditional/positive.java
@@ -1,8 +1,10 @@
 class Test{
 	public static void main(String[] args){
-	int a=10;
-		if(a=+a){
+	int a=-10;
+		if(a>=0){
 		System.out.println("the given number is positive number");
-	}
+		}
+		else
+		System.out.println("the given number is negitive number");
 	}
 }
