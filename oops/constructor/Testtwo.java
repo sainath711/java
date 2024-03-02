ab6bce72965e03044584383620a3301d4646cfd4
sainath711 <sainathsaii007@gmail.com>
2024-03-02 14:09:56 +0530

    java

--- oops/constructor/Testtwo.java
+++ oops/constructor/Testtwo.java
@@ -17,17 +17,3 @@ class Test{
 
 
 
-/*class Emp{
-    int eId;
-    Emp(){
-        super();
-        //this.eId = 0;
-    }
-    public static void main(String[] args){
-
-       Emp e1=new Emp();
-    
-       System.out.println(e1);
-       System.out.println(e1.eId);
-    }
-}*/
