b530a4628846d8616d31f83e1c6775b78af62a56
sainath711 <sainathsaii007@gmail.com>
2024-03-02 14:07:47 +0530

    java

---
+++ oops/constructor/Testtwo.java
@@ -0,0 +1,33 @@
+//Default constructor
+class Test{
+		int id;
+		String name;
+		Test(){
+			//super();
+		//this.id=id;
+		//this.name=name;
+		}
+	public static void main(String[] args){
+	Test t2=new Test();
+	System.out.println(t2.id);
+	System.out.println(t2.name);
+	}
+}
+
+
+
+
+/*class Emp{
+    int eId;
+    Emp(){
+        super();
+        //this.eId = 0;
+    }
+    public static void main(String[] args){
+
+       Emp e1=new Emp();
+    
+       System.out.println(e1);
+       System.out.println(e1.eId);
+    }
+}*/
