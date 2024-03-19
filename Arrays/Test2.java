db8e59ce9ba7cd13074de23af219939e8fdbcde7
sainath711 <sainathsaii007@gmail.com>
2024-03-19 11:26:09 +0530

    java

--- Arrays/Test2.java
+++ Arrays/Test2.java
@@ -1,9 +1,16 @@
 class test{
 	public static void main(String[] args){
-		int[] i={100,200,300,400,500};
+		int[] i={100,200,300,400};
 		String[] enames={"sai","shiva","raju","khanna"};
-		for(i=0;i<=4;i++);
-		System.out.println(i);
+		//for(i=0;i<=4;i++);
+		System.out.println(i[0]);
+		System.out.println(enames[0]);
+		System.out.println(i[1]);
+		System.out.println(enames[1]);
+		System.out.println(i[2]);
+		System.out.println(enames[2]);
+		System.out.println(i[3]);
+		System.out.println(enames[3]);
 		
 		
 		
---
+++ control flow stmts/conditional/Seven.java
@@ -0,0 +1,8 @@
+class Test{
+	public static void main(String[] args){
+	int i=49;
+	if(i%7== 0){
+	System.out.println("given number is divisible by 7");
+	}
+	}
+}
