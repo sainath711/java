c12fd1a2cf0924c0fcee4f74193672616e75d0ed
sainath711 <sainathsaii007@gmail.com>
2024-05-05 19:29:18 +0530

    java

---
+++ src/arrays/lengthTest.java
@@ -0,0 +1,8 @@
+package arrays;
+
+public class lengthTest {
+    public static void main(String[] args) {
+        int[] a={10,20,30};
+        System.out.println(a.length);
+    }
+}
---
+++ src/arrays/stringLength.java
@@ -0,0 +1,8 @@
+package arrays;
+
+public class stringLength {
+    public static void main(String[] args) {
+        String a="sainath";
+        System.out.println(a.length());
+    }
+}
