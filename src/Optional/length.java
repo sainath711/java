f4fa2ac62b7ead85253a123404ced81536028b1f
sainath711 <sainathsaii007@gmail.com>
2024-05-07 17:48:13 +0530

    java

---
+++ src/Optional/length.java
@@ -0,0 +1,16 @@
+package Optional;
+
+public class length {
+    public static void main(String[] args) {
+        int[] a=new int[100];
+        String[] b=new String[100];
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println(a.length);
+        System.out.println(b.length);
+        String c="sai nath";
+        System.out.println(c.length());
+        System.out.println(c.toUpperCase());
+    }
+}
