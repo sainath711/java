3194567b7c4d2b63c50a67bb1bf9a7ad006ffd67
sainath711 <sainathsaii007@gmail.com>
2024-06-03 12:50:55 +0530

    java

---
+++ src/oops/cmpltoops/Testone.java
@@ -0,0 +1,28 @@
+package oops.cmpltoops;
+
+public class Testone {
+    public static void main(String[] args) {
+        SavingsAccount SA=new SavingsAccount();
+        SA.setAcc_id(101);
+        SA.setAcc_name("RAHUL");
+        SA.setAcc_bal(30000);
+        SA.cal_Bal();
+        /*System.out.println(SA.getMin_bal());
+        System.out.println(SA.getAcc_id());
+        System.out.println(SA.getAcc_name());
+        System.out.println(SA.getAcc_bal());*/
+        System.out.println(SA.cal_Bal());
+
+
+        CurrentAccount CA=new CurrentAccount();
+        CA.setAcc_id(102);
+        CA.setAcc_name("SONIA");
+        CA.setAcc_bal(20000);
+        CA.cal_Bal();
+        /*System.out.println(CA.getMin_bal());
+        System.out.println(CA.getAcc_id());
+        System.out.println(CA.getAcc_name());
+        System.out.println(CA.getAcc_bal());*/
+        System.out.println(CA.cal_Bal());
+    }
+}
