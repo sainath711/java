c3e0c80b845bad533ed91cee68054d17593d2f5f
sainath711 <sainathsaii007@gmail.com>
2024-06-04 23:04:35 +0530

    java

---
+++ src/oops/cmpltoops/AccountService.java
@@ -0,0 +1,7 @@
+package oops.cmpltoops;
+
+public class AccountService {
+    public static void get_Service(Account a){
+        System.out.println(a.cal_Bal());
+    }
+}
--- src/oops/cmpltoops/Testone.java
+++ src/oops/cmpltoops/Testone.java
@@ -7,10 +7,10 @@ public class Testone {
         SA.setAcc_name("RAHUL");
         SA.setAcc_bal(30000);
         SA.cal_Bal();
-        /*System.out.println(SA.getMin_bal());
+        System.out.println(SA.getMin_bal());
         System.out.println(SA.getAcc_id());
         System.out.println(SA.getAcc_name());
-        System.out.println(SA.getAcc_bal());*/
+        System.out.println(SA.getAcc_bal());
         System.out.println(SA.cal_Bal());
 
 
@@ -19,10 +19,12 @@ public class Testone {
         CA.setAcc_name("SONIA");
         CA.setAcc_bal(20000);
         CA.cal_Bal();
-        /*System.out.println(CA.getMin_bal());
+        System.out.println(CA.getMin_bal());
         System.out.println(CA.getAcc_id());
         System.out.println(CA.getAcc_name());
-        System.out.println(CA.getAcc_bal());*/
+        System.out.println(CA.getAcc_bal());
         System.out.println(CA.cal_Bal());
+        AccountService.get_Service(SA);
+        AccountService.get_Service(CA);
     }
 }
