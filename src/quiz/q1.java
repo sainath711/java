8b7f5087faaf0c2832d202e340a67cc6575a2f58
sainath711 <sainathsaii007@gmail.com>
2024-06-10 12:11:54 +0530

    java

---
+++ src/quiz/q1.java
@@ -0,0 +1,10 @@
+package quiz;
+
+public class q1 {
+    public static void main(String[] args) {
+        String[] strings={"java","Python","javascript","Html"};
+        String languages=String.join(":",strings);
+
+        System.out.println(languages);
+    }
+}
