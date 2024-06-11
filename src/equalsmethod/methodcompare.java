b46d2bcceadd5b4b315aa2715285bbeff1d14772
sainath711 <sainathsaii007@gmail.com>
2024-06-11 17:02:00 +0530

    java

--- src/equalsmethod/methodcompare.java
+++ src/equalsmethod/methodcompare.java
@@ -7,10 +7,11 @@ public class methodcompare {
        String s3="Rahul";
        String s4=new String("Rahul");
 
-       //for String equals method is content comparision
+       //for String equals method is content comparision and for objects reference comparision
         System.out.println(s1.equals(s2));//true
         System.out.println(s1.equals(s4));//true
         System.out.println(s1.equals(s3));//true
+        System.out.println(s2.equals(s3));//true
 
     }
 }
