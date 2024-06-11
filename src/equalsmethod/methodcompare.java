09aa179abe485adf725196fe467d3077c237bbd9
sainath711 <sainathsaii007@gmail.com>
2024-06-11 16:14:57 +0530

    java

---
+++ src/equalsmethod/methodcompare.java
@@ -0,0 +1,16 @@
+package equalsmethod;
+
+public class methodcompare {
+    public static void main(String[] args) {
+       String s1=new String("Rahul");
+       String s2="Rahul";
+       String s3="Rahul";
+       String s4=new String("Rahul");
+
+       //for String equals method is content comparision
+        System.out.println(s1.equals(s2));//true
+        System.out.println(s1.equals(s4));//true
+        System.out.println(s1.equals(s3));//true
+
+    }
+}
---
+++ src/quiz/newstring1.java
@@ -0,0 +1,8 @@
+package quiz;
+
+public class newstring1 {
+    public static void main(String[] args) {
+        String s2=new String("hello");
+        System.out.println(s2);
+    }
+}
