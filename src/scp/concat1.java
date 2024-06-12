20b80f0a2bd3dee09391cd989436878052d3b14d
sainath711 <sainathsaii007@gmail.com>
2024-06-12 12:09:17 +0530

    java

--- src/scp/concat1.java
+++ src/scp/concat1.java
@@ -2,6 +2,7 @@ package scp;
 
 public class concat1 {
 
+    //String is immutable
         public static void main(String[] args) {
             String s1="Rahul";
            s1= s1.concat("Gandhi");
--- src/scp/stringbuffer.java
+++ src/scp/stringbuffer.java
@@ -1,6 +1,8 @@
 package scp;
 
 public class stringbuffer {
+
+    //Stringbuffer is muttable
     public static void main(String[] args) {
         StringBuffer s1=new StringBuffer("Rahul");
         s1.append("Gandhi");
