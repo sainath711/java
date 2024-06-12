aebafb3c566d8b2b2f6bf6b5988b80ca9c7d9bb2
sainath711 <sainathsaii007@gmail.com>
2024-06-12 12:00:03 +0530

    java

---
+++ src/scp/stringbuffer.java
@@ -0,0 +1,9 @@
+package scp;
+
+public class stringbuffer {
+    public static void main(String[] args) {
+        StringBuffer s1=new StringBuffer("Rahul");
+        s1.append("Gandhi");
+        System.out.println(s1);
+    }
+}
