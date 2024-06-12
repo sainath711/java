8f1715fa59a2fb1c9ea9d20b9f32b7db11831021
sainath711 <sainathsaii007@gmail.com>
2024-06-12 12:04:20 +0530

    java

---
+++ src/scp/concat2.java
@@ -0,0 +1,10 @@
+package scp;
+
+public class concat2 {
+    public static void main(String[] args) {
+        String s1=new String("Rahul");
+        String s2= s1.concat("Gandhi");
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
