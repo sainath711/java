bbbe0de6b57a2bce722daf9e695de85e8f345279
sainath711 <sainathsaii007@gmail.com>
2024-06-12 11:55:46 +0530

    java

---
+++ src/scp/concat.java
@@ -0,0 +1,9 @@
+package scp;
+
+public class concat {
+    public static void main(String[] args) {
+        String s1="Rahul";
+        s1.concat("Gandhi");
+        System.out.println(s1);
+    }
+}
