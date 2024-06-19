6e3aa03ea76fd15bbfa5fad7c36a332c74549f3b
sainath711 <sainathsaii007@gmail.com>
2024-06-19 23:43:58 +0530

    Codechef

---
+++ src/codechef/condition.java
@@ -0,0 +1,20 @@
+package codechef;
+import java.util.*;
+public class condition {
+        //conditional statement taking user input
+
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            // your code goes here
+            Scanner sc=new Scanner(System.in);
+            int t=sc.nextInt();
+            if(t==15){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+
+        }
+
+}
