d9ae485e6a21abbd9c6ca5e3b8def496a784040c
sainath711 <sainathsaii007@gmail.com>
2024-06-19 12:04:41 +0530

    catched

---
+++ src/Exceptionhandling/CheckedAndUnchecked/TestOne.java
@@ -0,0 +1,17 @@
+package Exceptionhandling.CheckedAndUnchecked;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class TestOne {
+    //Checked
+    public static void main(String[] args) {
+        try {
+            PrintWriter pw = new PrintWriter("TestOne.text");
+            System.out.println("GM");
+        }
+        catch (FileNotFoundException fe){
+            System.out.println(fe.getMessage());
+        }
+    }
+}
