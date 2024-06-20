a78cbba2f37fa517543bb353e7ddba52a6b24731
sainath711 <sainathsaii007@gmail.com>
2024-06-20 23:23:50 +0530

    Codechef

---
+++ src/codechef/weekdays.java
@@ -0,0 +1,17 @@
+package codechef;
+
+import java.util.Scanner;
+
+import java.util.*;
+public class weekdays {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=7;
+        System.out.println(c-(a+b));
+
+    }
+}
