72d238556cf1f1799d294919de5b79062d774ba9
sainath711 <sainathsaii007@gmail.com>
2024-06-20 23:07:41 +0530

    Codechef

---
+++ src/codechef/days.java
@@ -0,0 +1,17 @@
+package codechef;
+import java.util.*;
+public class days {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        if(t==4){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+
+    }
+}
