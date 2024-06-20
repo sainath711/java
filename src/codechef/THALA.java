3b73eef8d92964e389c54d561424c1e3b7b33f95
sainath711 <sainathsaii007@gmail.com>
2024-06-20 23:36:27 +0530

    THALA for a reason

---
+++ src/codechef/THALA.java
@@ -0,0 +1,20 @@
+package codechef;
+
+
+import java.util.Scanner;
+
+public class THALA {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        if(N==7){
+            System.out.println("THALA");
+        }
+        else{
+            System.out.println("SADGE");
+        }
+
+    }
+}
