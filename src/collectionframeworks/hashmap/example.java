48482dc4f778ab0918e454319aa90fbb498f1720
sainath711 <sainathsaii007@gmail.com>
2024-07-03 12:14:27 +0530

    HashMap

---
+++ src/collectionframeworks/hashmap/example.java
@@ -0,0 +1,13 @@
+package collectionframeworks.hashmap;
+
+import java.util.HashMap;
+
+public class example {
+    public static void main(String[] args) {
+        HashMap<String,String> emp=new HashMap<String,String>();
+        emp.put("id","101");
+        emp.put("name","Rahul");
+        emp.put("sal","45000");
+        System.out.println(emp);
+    }
+}
