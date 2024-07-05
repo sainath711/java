cfb237e6f16baa6e548ffd2d2832a4f9b921ffd5
sainath711 <sainathsaii007@gmail.com>
2024-07-05 12:16:28 +0530

    Functional Interface Example

--- src/functionalInterface/FI/Daoi.java
+++ src/functionalInterface/FI/Daoi.java
@@ -1,7 +1,8 @@
 package functionalInterface.FI;
-
+//Functional Interface with 2 methods without restricting the FI
 public interface Daoi {
     public abstract void login();
+    public abstract void logout();
 }
 class Daoiimpl implements Daoi{
     @Override
@@ -9,8 +10,14 @@ class Daoiimpl implements Daoi{
         System.out.println("Login Success");
     }
 
+    @Override
+    public void logout() {
+        System.out.println("Logout success");
+    }
+
     public static void main(String[] args) {
         Daoi dao=new Daoiimpl();
         dao.login();
+        dao.logout();
     }
 }
