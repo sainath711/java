788d47779611d11e98a00e68770537c422fd86a0
sainath711 <sainathsaii007@gmail.com>
2024-07-13 11:37:55 +0530

    printing even numbers using Stream

---
+++ src/StreamApi1/evenStreamCollect.java
@@ -0,0 +1,14 @@
+package StreamApi1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class evenStreamCollect {
+    public static void main(String[] args) {
+        List<Integer> numbers= Arrays.asList(10,20,30,31,21,11);
+        List even_numbers=numbers.stream().filter(number->number%2==0)
+                .collect(Collectors.toList());
+        System.out.println(even_numbers);
+    }
+}
