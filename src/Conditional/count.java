07bd157edb05b6887c5e4452011b5e775b389e84
sainath711 <sainathsaii007@gmail.com>
2024-08-05 17:03:33 +0530

    Conditional Statements with while

---
+++ src/Conditional/count.java
@@ -0,0 +1,14 @@
+package Conditional;
+
+public class count {
+    public static void main(String[] args) {
+        int countdown=3;
+        while (countdown>0){
+            System.out.println(countdown);
+            countdown--;
+        }
+
+        System.out.println("Happy new year!!");
+
+    }
+}
