a0bd19aeed6d28e8b3b7677c1b65288967116d89
sainath711 <sainathsaii007@gmail.com>
2024-08-05 17:03:43 +0530

    Conditional Statements with for

---
+++ src/Conditional/countfor.java
@@ -0,0 +1,12 @@
+package Conditional;
+
+public class countfor {
+    public static void main(String[] args) {
+        for (int i = 3; i > 0; i--) {
+            System.out.println(i);
+
+
+        }
+        System.out.println("Happy New Year!!");
+    }
+}
