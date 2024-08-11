d30ae75d630e36de1a5a096069aca691116b2598
sainath711 <sainathsaii007@gmail.com>
2024-08-11 18:07:36 +0530

    example on Ternary Problem

---
+++ src/Conditional/Ternary/one.java
@@ -0,0 +1,9 @@
+package Conditional.Ternary;
+
+public class one {
+    public static void main(String[] args) {
+        int time = 20;
+        String result = (time < 18) ? "Good day." : "Good evening.";
+        System.out.println(result);
+    }
+}
