1b294f1df1b315767df38978723823d0bd4b735c
sainath711 <sainathsaii007@gmail.com>
2024-08-12 22:55:23 +0530

    example on math Problem

---
+++ src/math/math2.java
@@ -0,0 +1,16 @@
+package math;
+
+public class math2 {
+    public static void main(String[] args) {
+        int a=-10;
+        int b=-25;
+        int c=Math.min(a,b);
+        int d=Math.max(a,b);
+        int e=Math.abs(a);
+        double f=Math.sqrt(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
