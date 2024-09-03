05459f4dc642a158df6273108a8882c456f6ba00
sainath711 <sainathsaii007@gmail.com>
2024-09-03 12:42:58 +0530

    variables

---
+++ src/variables/widening.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class widening {
+    public static void main(String[] args) {
+        int a=10;
+        double d=a;
+        float f=a; //int can convert  to float
+       // int c=f; float can't convert to int
+         double b=f;
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(b);
+        System.out.println(d);
+        //System.out.println(c);
+    }
+}
