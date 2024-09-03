360fb09ceb50a2302e7e1383577459a39cebc691
sainath711 <sainathsaii007@gmail.com>
2024-09-03 18:09:29 +0530

    counting number of digits in a number

---
+++ src/Loops/count.java
@@ -0,0 +1,14 @@
+package Loops;
+//count a number
+public class count {
+    public static void main(String[] args) {
+        int n=23456;
+        int res=0;
+        while (n>0){
+            n=n/10;
+            res++;
+        }
+        System.out.println(res);
+
+    }
+}
