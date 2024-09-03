4efa04eb59361c763cdc1a88a9b79826d058065d
sainath711 <sainathsaii007@gmail.com>
2024-09-03 15:22:52 +0530

    lowertype

---
+++ src/variables/lowertype.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class lowertype {
+    public static void main(String[] args) {
+        byte a=10;
+        byte b=10;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
