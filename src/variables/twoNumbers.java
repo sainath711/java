d20ad9723036ac59be47a3974578d68e4960d1f5
sainath711 <sainathsaii007@gmail.com>
2024-09-03 12:37:20 +0530

    variables

---
+++ src/variables/twoNumbers.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class twoNumbers {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20+a;
+        int c=a+b;
+        int d=b;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
