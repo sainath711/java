a178e7180eb0fa9857266dc527e7947defc54e0a
sainath711 <sainathsaii007@gmail.com>
2024-09-03 15:06:56 +0530

    explicit typecasting

---
+++ src/Typecasting/explicit/exp.java
@@ -0,0 +1,10 @@
+package Typecasting.explicit;
+
+public class exp {
+    public static void main(String[] args) {
+        double d=10;
+        int i=(int) d;//double can't convert into int
+        System.out.println(d);
+        System.out.println(i);
+    }
+}
