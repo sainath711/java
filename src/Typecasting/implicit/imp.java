587d684b43d7807acd94dbcaf3f6f1b176788cd8
sainath711 <sainathsaii007@gmail.com>
2024-09-03 13:31:14 +0530

    implicit typecasting

---
+++ src/Typecasting/implicit/imp.java
@@ -0,0 +1,10 @@
+package Typecasting.implicit;
+//implicit typecating:compiler can automatically converts
+public class imp {
+    public static void main(String[] args) {
+        int a=10;
+        double d=a;//int can convert into double
+        System.out.println(a);
+        System.out.println(d);
+    }
+}
