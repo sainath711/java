5f7e716161fc696637da28b1eb0ebc7d8cc23006
sainath711 <sainathsaii007@gmail.com>
2024-09-04 18:07:05 +0530

    All Variables

---
+++ src/variables/AllVaraibles.java
@@ -0,0 +1,19 @@
+package variables;
+
+
+
+
+public class AllVaraibles {
+    public static void main(String[] args) {
+    int num = 10;
+    float num1 = 233f;
+    double num2 = 344d;
+    char name = 'd';
+    String ename = "Sainath";
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(name);
+        System.out.println(ename);
+}
+}
