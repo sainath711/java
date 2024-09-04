c04736bc2036aed70b32770682e6c62beac31757
sainath711 <sainathsaii007@gmail.com>
2024-09-04 16:37:10 +0530

    variables

---
+++ src/variables/check.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class check {
+    public static void main(String[] args) {
+        int mynum=10;
+       mynum=15;
+        mynum=20;
+        System.out.println(mynum);
+    }
+}
