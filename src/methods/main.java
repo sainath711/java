0542d8b1a60e06c4a45cd0559a5c5f91c7a64e37
sainath711 <sainathsaii007@gmail.com>
2024-09-16 11:05:19 +0530

    without static method

---
+++ src/methods/main.java
@@ -0,0 +1,13 @@
+package methods;
+
+public class main {
+     static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+
+    public static void main(String[] args) {
+         main m1=new main();
+        m1.myMethod();
+    }
+
+}
