49ed68969da3c6d1c65f687b75df26874ed3b227
sainath711 <sainathsaii007@gmail.com>
2024-09-16 13:12:01 +0530

    parameters with  return value

---
+++ src/parameters/returnValues/exReturn.java
@@ -0,0 +1,14 @@
+package parameters.returnValues;
+
+public class exReturn {
+    static int m1(int x,int y){
+        return y+x;
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(m1(10,20));
+
+    }
+}
