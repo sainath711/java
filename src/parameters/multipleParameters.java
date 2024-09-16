ee2ea4826d05d8d63186ff49bf4d4bcef1d5aab2
sainath711 <sainathsaii007@gmail.com>
2024-09-16 11:26:27 +0530

    multiple parameters with multiple arguments

---
+++ src/parameters/multipleParameters.java
@@ -0,0 +1,12 @@
+package parameters;
+
+public class multipleParameters {
+    static void m1(String name,int age){
+        System.out.println(name  +" Age is " +   age  );
+    }
+
+    public static void main(String[] args) {
+        m1("Sai", 10);
+        m1("Ramani",12);
+    }
+}
