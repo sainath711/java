8b2830f13bbad49038737244fbe56ace4856dceb
sainath711 <sainathsaii007@gmail.com>
2024-09-18 11:16:29 +0530

    class with two objects

---
+++ src/oops/Class/two.java
@@ -0,0 +1,14 @@
+package oops.Class;
+
+public class two {
+    static int c=15;
+    public static void main(String[] args) {
+        one obj=new one();
+        one obj1=new one();
+        System.out.println(obj.a);
+        System.out.println(obj.b);
+        System.out.println(obj1.b);
+        System.out.println(obj1.a);
+        System.out.println(c);
+    }
+}
