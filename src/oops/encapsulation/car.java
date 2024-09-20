5e633045e0345c8b8cad0c7aad8f3d398fe402dd
sainath711 <sainathsaii007@gmail.com>
2024-09-20 11:53:11 +0530

    Encapsulation example

---
+++ src/oops/encapsulation/car.java
@@ -0,0 +1,15 @@
+package oops.encapsulation;
+
+public class car {
+    String model;
+    int year;
+   private String colour;
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+}
