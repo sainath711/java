b612db0af75190ba07cd2265c3fac75feccc321b
sainath711 <sainathsaii007@gmail.com>
2024-09-22 23:02:15 +0530

    Counting a number

--- src/Loops/count.java
+++ src/Loops/count.java
@@ -2,7 +2,7 @@ package Loops;
 //count a number
 public class count {
     public static void main(String[] args) {
-        int n=23456;
+        int n=2345;
         int res=0;
         while (n>0){
             n=n/10;
