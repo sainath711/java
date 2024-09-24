df7e1831fbf8e356831cf1d888a08e38bd6a5c55
sainath711 <sainathsaii007@gmail.com>
2024-09-24 16:50:26 +0530

    multiple inheritance using interfaces

---
+++ src/interface2/muitipleInheritance/main.java
@@ -0,0 +1,10 @@
+package interface2.muitipleInheritance;
+
+public class main {
+    public static void main(String[] args) {
+        Cat obj=new Cat();
+        obj.Eat();
+        obj.Run();
+        obj.Jump();
+    }
+}
