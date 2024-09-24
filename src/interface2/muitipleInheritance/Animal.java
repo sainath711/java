c883b11ec0239100c261089a14868a87dc0e7548
sainath711 <sainathsaii007@gmail.com>
2024-09-24 17:05:24 +0530

    multiple inheritance using interfaces

--- src/interface2/muitipleInheritance/Animal.java
+++ src/interface2/muitipleInheritance/Animal.java
@@ -3,4 +3,5 @@ package interface2.muitipleInheritance;
 public abstract interface Animal {
     public abstract void Run();
     public abstract void Eat();
+
 }
--- src/interface2/muitipleInheritance/main.java
+++ src/interface2/muitipleInheritance/main.java
@@ -6,5 +6,6 @@ public class main {
         obj.Eat();
         obj.Run();
         obj.Jump();
+        
     }
 }
