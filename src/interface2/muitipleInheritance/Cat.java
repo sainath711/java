9b5281d213c377023a602910220b2d707ba67aa2
sainath711 <sainathsaii007@gmail.com>
2024-09-25 12:10:37 +0530

    multiple inheritance using interfaces

--- src/interface2/muitipleInheritance/Cat.java
+++ src/interface2/muitipleInheritance/Cat.java
@@ -1,18 +1,5 @@
 package interface2.muitipleInheritance;
 
-public class Cat implements Animal,Dog{
-    @Override
-    public void Eat() {
-        System.out.println("Dog can eat");
-    }
-
-    @Override
-    public void Run() {
-        System.out.println("Dog can Run");
-    }
-
-    @Override
-    public void Jump() {
-        System.out.println("Dog can jump");
-    }
+public interface Cat {
+    void catjump();
 }
