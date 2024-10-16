583cbd659413ac31e926ad3171294c60d0cc741e
sainath711 <sainathsaii007@gmail.com>
2024-10-16 19:16:38 +0530

    first 10 natural numbers

---
+++ src/Loops/For/natural10.java
@@ -0,0 +1,13 @@
+package Loops.For;
+
+public class natural10 {
+    public static void main(String[] args) {
+        int sum=0;
+        int i=1;
+        while (i<=10){
+           sum=sum+i;
+           i++;
+        }
+        System.out.println(sum);
+    }
+}
