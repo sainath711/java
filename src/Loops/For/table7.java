641092d66e986ffe0d08872f6e87519ce4330c09
sainath711 <sainathsaii007@gmail.com>
2024-10-16 19:17:01 +0530

    7th table

---
+++ src/Loops/For/table7.java
@@ -0,0 +1,10 @@
+package Loops.For;
+
+public class table7 {
+    public static void main(String[] args) {
+        for (int i =1; i <=10 ; i++) {
+            System.out.println("7*"+ i+"="+i*7);
+
+        }
+    }
+}
