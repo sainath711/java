23176fee536032af92fe7426aa75bd726da251a2
sainath711 <sainathsaii007@gmail.com>
2024-10-16 13:27:16 +0530

    Dowhile example

---
+++ src/Loops/DoWhile/ex1.java
@@ -0,0 +1,14 @@
+package Loops.DoWhile;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+            //int i=11; without checking condition it will execute once 11
+            int i=0;
+            do {
+                System.out.println(i);
+                i++;
+            }
+            while (i<=10);
+    }
+}
