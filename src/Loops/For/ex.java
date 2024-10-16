947ab86c665fff8f96222392638c09c676e83f8e
sainath711 <sainathsaii007@gmail.com>
2024-10-16 13:27:01 +0530

    for loop example

---
+++ src/Loops/For/ex.java
@@ -0,0 +1,11 @@
+package Loops.For;
+
+public class ex {
+    public static void main(String[] args) {
+        String a="Hello world";
+        for (int i = 0; i <100 ; i++) {
+            System.out.println(a);
+
+        }
+    }
+}
