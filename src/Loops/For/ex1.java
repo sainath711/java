d1da28c7fe9d854605211ab2e9ca4b7d51357652
sainath711 <sainathsaii007@gmail.com>
2024-11-06 13:22:41 +0530

    Youtube Example Program

---
+++ src/Loops/For/ex1.java
@@ -0,0 +1,10 @@
+package Loops.For;
+
+public class ex1 {
+    public static void main(String[] args) {
+        for (int i = 1,j=1; j<=6;i=i*10,j++) {
+            System.out.println(i);
+
+        }
+    }
+}
