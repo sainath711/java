cdf1956cde4395039208136349b2ea3c80610dd9
sainath711 <sainathsaii007@gmail.com>
2024-11-08 13:36:52 +0530

    After 1.8 interface allows Static default methods

---
+++ src/Optional/TestThree.java
@@ -0,0 +1,11 @@
+package Optional;
+//After 1.8 interface allows Static and Default methods
+public interface TestThree {
+    public static void m1(){
+        System.out.println("GN");
+    }
+
+    public static void main(String[] args) {
+        m1();
+    }
+}
