6cc8166b81dc4aca33ed3056318f900817d0fdd6
sainath711 <sainathsaii007@gmail.com>
2024-11-08 13:51:49 +0530

    predefined FI with Lambda

--- src/java81/predicate/TestOne.java
+++ src/java81/predicate/TestOne.java
@@ -2,6 +2,7 @@ package java81.predicate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class TestOne {
@@ -9,6 +10,8 @@ public class TestOne {
         List<Integer> al= Arrays.asList(10,20,30);
         System.out.println(al);
         Predicate<List> obj1=numbers->numbers.isEmpty();
+        Function<List,Integer> obj2=numbers->numbers.size();
         System.out.println(obj1.test(al));
+        System.out.println(obj2.apply(al));
     }
 }
