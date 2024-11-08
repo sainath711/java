80812313bbcbd58ea43d7c60da2d369800959813
sainath711 <sainathsaii007@gmail.com>
2024-11-08 13:50:03 +0530

    separating a list of numbers into odd and even

--- src/StreamApi1/evenStreamCollect.java
+++ src/StreamApi1/evenStreamCollect.java
@@ -9,6 +9,8 @@ public class evenStreamCollect {
         List<Integer> numbers= Arrays.asList(10,20,30,31,21,11);
         List even_numbers=numbers.stream().filter(number->number%2==0)
                 .collect(Collectors.toList());
+        List Odd_numbers=numbers.stream().filter(number->number%2!=0).collect(Collectors.toList());
         System.out.println(even_numbers);
+        System.out.println(Odd_numbers);
     }
 }
