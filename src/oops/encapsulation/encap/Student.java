a2a6dacf0226d9f82210c695230f561acb8f9f16
sainath711 <sainathsaii007@gmail.com>
2025-01-22 09:28:37 +0530

    Encapsulation Example

---
+++ src/oops/encapsulation/encap/Student.java
@@ -0,0 +1,16 @@
+package oops.encapsulation.encap;
+
+public class Student {
+    int age;
+   private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    int rollnumber;
+}
