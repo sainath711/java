b3fd90dad54a9568a1d29d9dae8a2f2860423110
sainath711 <sainathsaii007@gmail.com>
2025-01-23 12:46:23 +0530

    Encapsulation Example

---
+++ src/Practice/Oops/Encapsulation/Student.java
@@ -0,0 +1,16 @@
+package Practice.Oops.Encapsulation;
+
+public class Student {
+    private int Age;
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    String Name;
+    int RollNumber;
+}
---
+++ src/Practice/Oops/Encapsulation/Test.java
@@ -0,0 +1,11 @@
+package Practice.Oops.Encapsulation;
+
+public class Test {
+    public static void main(String[] args) {
+        Student Std=new Student();
+        Std.setAge(24);
+        Std.Name="Sainath";
+        Std.RollNumber=42;
+        System.out.println(Std.getAge());
+    }
+}
